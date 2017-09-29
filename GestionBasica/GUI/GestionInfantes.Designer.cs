049ea038e3955883c0910c2dffb7e7e833d02007
tonloy <manciatonloy@hotmail.com>
2017-09-29 15:31:06 -0600

    hover de botones de gestion

--- GestionBasica/GUI/GestionInfantes.Designer.cs
+++ GestionBasica/GUI/GestionInfantes.Designer.cs
@@ -59,6 +59,7 @@
             this.toolStripLabel1});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
             this.toolStrip1.Size = new System.Drawing.Size(894, 25);
             this.toolStrip1.TabIndex = 2;
             this.toolStrip1.Text = "toolStrip1";
