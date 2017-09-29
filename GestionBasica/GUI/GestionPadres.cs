c310f3d7700cf0740f0a0516c4039609c2bbca4d
Er1ck6aldamez <ebalmore.gs@gmail.com>
2017-09-29 15:45:03 -0600

    Modificar Padre

--- GestionBasica/GUI/GestionPadres.cs
+++ GestionBasica/GUI/GestionPadres.cs
@@ -76,7 +76,36 @@ namespace GestionBasica.GUI
 
         private void txbModificar_Click(object sender, EventArgs e)
         {
+            if (MessageBox.Show("¿Desea EDITAR el Padre seleccionado?", "Pregunta", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                try
+                {
+                    PadreEdicion frm = new PadreEdicion();
+                    frm.txtID.Text = dataGridView1.CurrentRow.Cells["idPadre"].Value.ToString();
+                    frm.txbNombreCompleto.Text = dataGridView1.CurrentRow.Cells["NombreCompleto"].Value.ToString();
+                    frm.txtConocidapor.Text = dataGridView1.CurrentRow.Cells["ConocidoPor"].Value.ToString();
+                    frm.mskTEdad.Text = dataGridView1.CurrentRow.Cells["Edad"].Value.ToString();
+                    frm.cbxDepartamentos.SelectedIndex = frm.cbxDepartamentos.FindStringExact(dataGridView1.CurrentRow.Cells["Departamento"].Value.ToString());
+                    frm.cbxMunicipio.SelectedIndex = frm.cbxMunicipio.FindStringExact(dataGridView1.CurrentRow.Cells["Municipio"].Value.ToString());
+                    frm.txtDomicilio.Text = dataGridView1.CurrentRow.Cells["Domicilio"].Value.ToString();
+                    frm.txtProfesion.Text = dataGridView1.CurrentRow.Cells["Profesion"].Value.ToString();
+                    frm.txtNacionalidad.Text = dataGridView1.CurrentRow.Cells["Nacionalidad"].Value.ToString();
+                    frm.mtxtDUI.Text = dataGridView1.CurrentRow.Cells["DUI"].Value.ToString();
 
+                    frm.mskTEdad.ReadOnly = true;
+                    frm.cbxDepartamentos.Enabled = false;
+                    frm.cbxMunicipio.Enabled = false;
+                    frm.txtDomicilio.ReadOnly = true;
+                    frm.txtNacionalidad.ReadOnly = true;
+                    frm.mtxtDUI.ReadOnly = true;
+                    frm.ShowDialog();
+                    CargarPadres();
+                }
+                catch
+                {
+                    MessageBox.Show("Por favor seleccione un Padre");
+                }
+            }
         }
 
         private void toolStripLabel1_Click(object sender, EventArgs e)
