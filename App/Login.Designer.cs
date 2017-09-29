1c137a0e0026a76cfaa3498e5eea338979c3ac8e
tonloy <manciatonloy@hotmail.com>
2017-09-29 11:46:40 -0600

    cambie icono de inicio de sesion

--- App/Login.Designer.cs
+++ App/Login.Designer.cs
@@ -57,12 +57,13 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(260, 73);
+            this.pictureBox1.Location = new System.Drawing.Point(260, 70);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(25, 23);
+            this.pictureBox1.Size = new System.Drawing.Size(38, 31);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 14;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // btnSalir
             // 
@@ -144,7 +145,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(337, 253);
+            this.ClientSize = new System.Drawing.Size(337, 239);
             this.Controls.Add(this.pbxFranja);
             this.Controls.Add(this.chkContrasena);
             this.Controls.Add(this.pictureBox2);
