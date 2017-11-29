ac370d77f76b559b27b42e88956b3405d0a257e2
tonloy <manciatonloy@hotmail.com>
2017-11-29 10:30:04 -0600

    cache arreglado y partidas_matrimonio tambien

--- CacheManager1/Cache.cs
+++ CacheManager1/Cache.cs
@@ -1079,15 +1079,27 @@ namespace CacheManager1
 
             return Datos;
         }
-        
+
         public static DataTable ID_Papas_Conyuges(String idInfante)
         {
             DataTable Datos = new DataTable();
             String Consulta;
-            Consulta = "select pnac.idPadre , pnac.idMadre from partidas_nacimiento pnac where pnac.idInfante="+idInfante+";";
+            Consulta = "select pnac.idPadre , pnac.idMadre from partidas_nacimiento pnac where pnac.idInfante=" + idInfante + ";";
 
-        
-        public static DataTable ID_Papas_Conyuges(String idInfante)
+            DataLayer1.OperacionBD oOperacion = new DataLayer1.OperacionBD();
+            try
+            {
+                Datos = oOperacion.Consultar(Consulta);
+            }
+            catch
+            {
+                Datos = new DataTable();
+            }
+
+            return Datos;
+        }
+
+        public static DataTable EstadoFamiliar_Conyugue(String idInfante)
         {
             DataTable Datos = new DataTable();
             String Consulta;
